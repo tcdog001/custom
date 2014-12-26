44cf978741ef3076711bb23b0db00296c65e5534
liuhuijun <liuhuijun@e830.(none)>
2014-12-26 11:27:34 +0800

    modify

--- etc/upgrade/rsync_start_bymanual.sh
+++ etc/upgrade/rsync_start_bymanual.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-. ${__ROOTFS__}/etc/utils/utils.in
+. ${__ROOTFS__}/etc/upgrade/rsync.in
 
-#
-# atbus.autelan.com ==> 182.254.198.168
-#
-readonly -A deft_hash=(
-	[server]=atbus.autelan.com
-	[path]=/opt/version/lte-fi/mdboard
-	[port]=873
-	[user]=root
-	[pass]=ltefi@Autelan1
-)
+usage() {
+	echo "$0 version=xxx server=xxx path=xxx port=xxx user=xxx pass=xxx"
+	echo "		must input version/server/pass"
+}
 
 #
 # version=xxx server=xxx path=xxx port=xxx user=xxx pass=xxx
@@ -20,21 +14,15 @@ readonly -A deft_hash=(
 main() {
 	local -A hash
 
-	aa_init hash "$@"
+	aa_init hash "$@" || {
+		usage; reutrn ${e_inval}
+	}
 
 	if [[ -z "${hash[version]}" ]]; then
-		echo "$0 version=xxx server=xxx path=xxx port=xxx user=xxx pass=xxx"
-		echo "		must input version/server/pass"
-
-		return ${e_inval}
+		usage; reutrn ${e_inval}
 	fi
 
-	local key
-	for key in ${!deft_hash[@]}; do
-		if [[ -z "${hash[${key}]}" ]]; then
-			hash[${key}]="${deft_hash[${key}]}"
-		fi
-	done
+	aa_complete deft_rsync hash
 
 	${__ROOTFS__}/etc/upgrade/rsync_task.sh "$(json_from_aa hash)" &
 }
