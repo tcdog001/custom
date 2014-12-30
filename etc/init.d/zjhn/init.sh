c8b5209d6b4910935ed6e15e74a334e585e5646f
liuhuijun <liuhuijun@e830.(none)>
2014-12-30 15:56:06 +0800

    modify for zjhn

--- etc/init.d/zjhn/init.sh
+++ etc/init.d/zjhn/init.sh
@@ -4,9 +4,16 @@ main() {
 	local init=${__CP_SCRIPT__}/init.sh
 	if [[ ! -f "${init}" ]]; then
 		init=/etc/init.d/${__CP__}/init.script
-	fi
+        fi
 
-	${init} 
+        local err=0
+	for ((;;)); do
+		${init}; err=$?
+		if ((0==err)); then
+		        return
+		fi
+		sleep 60
+        done
 }
 
 main "$@"
