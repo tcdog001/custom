791ff2c8fa5461f07c3e0f53c8b696be616a2b14
liuhj <liuhuijun@debian.suns>
2015-01-15 04:58:08 -0500

    modify zjhn/init.sh when oem lsm is set

--- etc/init.d/zjhn/init.sh
+++ etc/init.d/zjhn/init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+URL_PATH=/etc/platform/conf/platform.json
+
 main() {
 	local init=${__CP_SCRIPT__}/init.sh
+	local default_url="{\"url\":\"https://atbus.autelan.com:8443/LMS/lte/\"}
+	local content_url=$(cat ${URL_PATH})
+	
+	[[ ${default_url} != ${content_url} ]] && return
+
 	if [[ ! -f "${init}" ]]; then
 		init=/etc/init.d/${__CP__}/init.script
         fi
