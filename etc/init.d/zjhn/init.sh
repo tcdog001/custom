a6743df64a85f79298441628a62b1dfde1b4d6c3
liuhj <liuhuijun@debian.suns>
2015-01-15 21:56:40 -0500

    modify zjhn init.sh init.script. when oem lms is set , init.scripts return 0 and exit

--- etc/init.d/zjhn/init.sh
+++ etc/init.d/zjhn/init.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-URL_PATH=/etc/platform/conf/platform.json
-
 main() {
 	local init=${__CP_SCRIPT__}/init.sh
-	local default_url="{\"url\":\"https://atbus.autelan.com:8443/LMS/lte/\"}"
-	local content_url=$(cat ${URL_PATH})
-	
-	[[ ${default_url} != ${content_url} ]] && return
 
 	if [[ ! -f "${init}" ]]; then
 		init=/etc/init.d/${__CP__}/init.script
@@ -15,11 +9,11 @@ main() {
 
         local err=0
 	for ((;;)); do
+		sleep 60
 		${init}; err=$?
 		if ((0==err)); then
 		        return
 		fi
-		sleep 60
         done
 }
 
