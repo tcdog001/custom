0604a21259f5e17d7fbca3249952d88f19c959c8
liuhj <liuhuijun@debian.suns>
2015-01-16 02:23:33 -0500

    modify 15 mins usr offline feature

--- usr/sbin/usr_offline.sh
+++ usr/sbin/usr_offline.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+. /etc/platform/bin/check_oem_function.sh
+
 main() {
+	local err=1
+	check_oem_lms; err=$?
+	[[ ${err} = 0 ]] && return ${err}
+
         iptables -t mangle -F WiFiDog_eth0.1_Trusted
 }
 main "$@"
