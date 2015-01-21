6fd057b77d80bcc860fae29406ed94faefcc0072
zhangsiyu <zhangsiyu@debian.suns>
2015-01-21 02:40:09 -0500

    modify by zhangsiyu for zjhn webupgrade 2015-1-21

--- etc/init.d/zjhn/init.sh
+++ etc/init.d/zjhn/init.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+. ${__ROOTFS__}/etc/utils/utils.in
+
 main() {
-	local init=${__CP_SCRIPT__}/init.sh
-
-	if [[ ! -f "${init}" ]]; then
-		init=/etc/init.d/${__CP__}/init.script
-        fi
-
-        local err=0
+	local init=/etc/init.d/${__CP__}/init.script
+	#
+	# 1: push du list
+	#
 	for ((;;)); do
+		${init} && break
+
 		sleep 60
-		${init}; err=$?
-		if ((0==err)); then
-		        return
-		fi
-        done
+	done
+
+	#
+	# 2: copy
+	#
+	if [[ ! -f /mnt/hd/custom/${__CP__}/script/init.sh && \
+			-f /usr/${__CP__}/script/init.sh ]]; then
+		cp -fpR /usr/${__CP__}/script/* /mnt/hd/custom/${__CP__}/script/
+	fi
+
+	#
+	# 3: call CP's init
+	#
+	init=${__CP_SCRIPT__}/init.sh
+	if [[ -f "${init}" ]]; then
+		${init}
+	fi
 }
 
 main "$@"
