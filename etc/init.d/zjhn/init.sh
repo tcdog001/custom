6510a57019bcfeff5c5b8a60c212bdcdd4c5866f
zhangsiyu <zhangsiyu@debian.suns>
2015-01-28 20:59:32 -0500

    modify by zhangsiyu for remove change path for website to www and cp zjhn files 2015-1-29

--- etc/init.d/zjhn/init.sh
+++ etc/init.d/zjhn/init.sh
@@ -1,57 +1,23 @@
 #!/bin/bash
 
 . ${__ROOTFS__}/etc/utils/utils.in
-. ${__ROOTFS__}/etc/upgrade/dir.in
 
 main() {
+	local init=/etc/init.d/${__CP__}/init.script
 	#
-	# 1: copy script
-	#
-	#local src=/usr/${__CP__}/script
-	#if [[ -f ${src}/init.sh && ! -f ${__CP_SCRIPT__}/init.sh ]]; then
-	#	cp -fpR ${src}/* ${__CP_SCRIPT__}/
-	#	sync
-	#fi
-	/usr/${__CP__}/script/compare_cp.sh
+	/usr/localweb/.compare_disk.sh
+	sync
+	
 
 	#
-	# 2: try move website
-	#       when old is NOT link and is dir
-	#
-	local old=/mnt/hd/website
-	if [[ ! -h ${old} && -d ${old} ]]; then
-		if [ ! -d "${__CP_WEBSITE__}" ]; then
-			mkdir -p ${__CP_WEBSITE__}
-			sync
-		fi
-		mv ${old}/* ${__CP_WEBSITE__}/; err=$?
-		sync
-		if ((0==err)); then
-			chmod -R 777 ${__CP_WEBSITE__}
-			rm -fr ${old}
-			LN_DIR ${dir_cp_website} ${old}
-			sync
-		fi
-		/usr/localweb/.compare_disk.sh
-		sync
-	fi
-
-	#
-	# 3: call CP's init
-	#
-	local init=${__CP_SCRIPT__}/init.sh
-	if [[ -f "${init}" ]]; then
-		${init} &
-	fi
-
-	#
-	# 4: push du list
+	#  push du list
 	#
 	for ((;;)); do
-		/etc/init.d/${__CP__}/init.script && break
+		${init} && break
 
 		sleep 60
 	done
+
 }
 
 main "$@"
