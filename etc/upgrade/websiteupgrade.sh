c8bb0f90ee656af37cedd1eb7bb657f57c3d5b49
ljf10000 <l_j_f@163.com>
2015-02-02 14:43:34 +0800

    websiteupgrade.sh support config

--- etc/upgrade/websiteupgrade.sh
+++ etc/upgrade/websiteupgrade.sh
@@ -2,6 +2,17 @@
 
 . ${__ROOTFS__}/etc/upgrade/dir.in
 
+readonly -A deft_website=(
+	[server]=lms9.autelan.com
+	[port]=873
+	[user]=rsync
+	[pass]="rsync4GOOD!"
+	[remote]=/opt/version/lte-fi/website
+	[timeout]=300
+)
+
+declare json_website
+
 #
 #$1:info
 #
@@ -39,18 +50,20 @@ website_upgrade_version() {
 }
 
 #
-#$1:remote
-#$2:dir
+#$1:remote dir/file
+#$2:local  dir/file
+#$3:aa
 #
 website_rsync() {
-	local remote=$1; remote=${remote%/}/
-	local dir=$2
+	local remote="$1"; remote=${remote%/}/
+	local dir="$2"
+	local website="$3"
 
-	local port="873"
-	local server="atbus.autelan.com"
-	local user="root"
-	local pass="ltefi@Autelan1"
-	local timeout="300"
+	local server="$(aa_get ${website} server)"
+	local port="$(aa_get ${website} port)"
+	local user="$(aa_get ${website} user)"
+	local pass="$(aa_get ${website} pass)"
+	local timeout="$(aa_get ${website} timeout)"
 
 	local sshparam="sshpass -p ${pass} ssh -l ${user} -o StrictHostKeyChecking=no"
 	local rsync_dynamic="--rsh=\"${sshparam}\" --timeout=${timeout}"
@@ -64,13 +77,14 @@ website_rsync() {
 	return ${err}
 }
 
-
 website_upgrade() {
-	local dir_remote=/opt/version/lte-fi/website
+	local website="$1"
+	local remote="$(aa_get ${website} remote)"
+
 	#
 	# get config
 	#
-	website_rsync ${dir_remote}/website_config ${dir_website_config} || return $?
+	website_rsync ${remote}/website_config ${dir_website_config} ${website} || return $?
 	if [[ ! -f "${file_website_config}" ]]; then
 		website_logger "no found ${file_website_config}"
 
@@ -93,15 +107,25 @@ website_upgrade() {
 	#
 	# do upgrade
 	#
-	website_rsync ${dir_remote}/${version} ${dir_website_upgrade} || return $?
+	website_rsync ${remote}/${version} ${dir_website_upgrade} ${website} || return $?
 	cp -fpR ${dir_website_upgrade} ${__CP_WEBSITE__}; sync
 }
 
 main() {
+	local deft=/etc/upgrade/config/website.json
+	local config=${dir_config_upgrade}/website.json
+
+	if [[ ! -f "${config}" ]]; then
+		cp -f ${deft} ${config}
+	fi
+
+	local -A website
+	json_to_aa_with_deft deft_website website "$(cat ${config})"
+
 	for ((;;)); do
 		sleep 600
 
-		website_upgrade && return
+		website_upgrade website && return
 	done
 }
 
