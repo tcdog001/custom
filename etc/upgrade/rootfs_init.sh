07fe024349db323cfd70ff4fa7b1e11f963fc6cc
liuhj <liuhuijun@debian.suns>
2015-02-05 23:55:37 -0500

    add #resetd in rootfs_init.sh

--- etc/upgrade/rootfs_init.sh
+++ etc/upgrade/rootfs_init.sh
@@ -3,7 +3,7 @@
 . ${__ROOTFS__}/etc/upgrade/upgrade.in
 
 main() {
-	${__ROOTFS__}/usr/sbin/resetd &
+#	${__ROOTFS__}/usr/sbin/resetd &
 	${__ROOTFS__}/etc/platform/bin/register.sh &
 	${__ROOTFS__}/etc/upgrade/websiteupgrade.sh &
 
