d4748a125dc25e1dfc8aa7accde8892bfa30a933
guoweibo <guoweibo@debian6.(none)>
2015-06-12 11:56:04 -0400

    renew command.sh

--- etc/platform/bin/command.sh
+++ etc/platform/bin/command.sh
@@ -7,6 +7,6 @@ main() {
 	local URL_PATH=/etc/platform/conf/platform.json
 	local URL_DEFAULT=https://atbus.97971com:68.8443/LMS/lte/
 
-	command_operation URL_tatle URL_PATH URL_DEFAULT
+	command_operation $URL_tatle $URL_PATH $URL_DEFAULT
 }
 main "$@"
