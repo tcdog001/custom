e6fc7f71520ca94dc8e6e0a4e6bf0c66ca9f943a
guoshuai <guoshuai@debian6.(none)>
2015-06-12 16:39:41 -0400

    modify upgrade_kernel.sh

--- etc/upgrade/upgrade_kernel.sh
+++ etc/upgrade/upgrade_kernel.sh
@@ -2,15 +2,21 @@
 
 . ${__ROOTFS__}/etc/upgrade/upgrade_kernel.in
 
+#
+# check the file /etc/.kversion and hi_kernel.bin
+# if the two files are exist, compare the time
+#
 do_upgrade() {
-    local ret=""
+    local version=$1
+    local ret_k=""
+    local ret_kv=""
 
-    check_kernel_file ${version}; ret=$?
-    if [[ ${ret} -eq 0 ]];then
+    check_kernel_file ${version}; ret_k=$?
+    check_kversion_file ${version}; ret_kv=$?
+    if [[ ${ret_k} -eq 0 && ${ret_kv} -eq 0 ]];then
         compare_uname_time ${version}
     else
-	    upgrade_echo_logger "kernel_upgrade" \
-        	"version=${version} hi_kernel.bin is not exist"
+        return 1
     fi
 }
 
@@ -19,7 +25,7 @@ main() {
     local upgrade_kernel=$2; shift 2
 
     if [[ "${upgrade_kernel}" == "yes" ]];then
-        do_upgrade
+        do_upgrade ${version}
     else
 	    upgrade_echo_logger "kernel_upgrade" \
         	"__UPGRADE_KERNEL__=no"
