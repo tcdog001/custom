757354ad0a9be3bb885aa84beaa1cdc546f1e287
liuhuijun <liuhj@autelan.com>
2016-01-04 18:01:03 +0800

    .

--- etc/um/iptables_init.sh
+++ etc/um/iptables_init.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-
 ipset -N white_list ipmap --network 192.168.0.0/16
+ipset -N dst_white hash:ip --hashsize 1024
+ipset -A dst_white 120.27.136.216 
+ipset -A dst_white 120.27.137.55 
+ipset -A dst_white 120.27.136.232 
+ipset -A dst_white 120.27.137.12 
+ipset -A dst_white 120.27.136.229 
+ipset -A dst_white 120.27.137.22 
+ipset -A dst_white 120.27.137.26 
+ipset -A dst_white 120.27.137.44 
+ipset -A dst_white 120.55.245.7 
+ipset -A dst_white 112.124.121.173 
 
-iptables -t nat -A PREROUTING  ! -d  192.168.0.1 -p tcp --dport 80 -i eth0.1  -m state --state NEW -m set --match-set white_list src -j REDIRECT --to-port 3126
-iptables -t nat -A PREROUTING  ! -d  192.168.0.1 -p tcp --dport 80 -i eth0.1  -m state --state NEW -j REDIRECT --to-port 3127 
+iptables -t nat -A PREROUTING  -i eth0.1  -m set --match-set dst_white dst  -j ACCEPT
+iptables -t nat -A PREROUTING  -i eth0    -m set --match-set dst_white src  -j ACCEPT
 
-iptables -P  FORWARD DROP
+iptables -t nat -A PREROUTING  -i eth0.1  -d 192.168.0.1  -j ACCEPT
 
-iptables -A FORWARD  -p udp --dport 53 -m state --state NEW -j ACCEPT
-
-iptables -A FORWARD  -i eth0.1 -o eth0 -m set --match-set  white_list src -j ACCEPT
-iptables -A FORWARD  -o eth0.1 -i eth0 -m set --match-set  white_list dst -j ACCEPT
-iptables -A FORWARD  -p icmp --icmp-type 8 -j ACCEPT
-#iptables -A FORWARD  -p tcp --dport 443 --syn -m state --state NEW -j ACCEPT
-#iptables -A FORWARD  -p tcp --dport 21 --syn -m state --state NEW -j ACCEPT
-#iptables -A FORWARD  -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A PREROUTING  -i eth0.1  -p tcp -j REDIRECT --to-port 3127
